8f0f1bb2f931b2e3bebd931e1b485206e085c8a9
Ekramul <mdekramulhassan168@gmail.com>
2025-01-20 21:03:14 +0600

    add hashpasswor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
-const check = "ok man";
-console.log(check);
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+export const prisma = new PrismaClient();
+
+interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+  userInfo: {
+    userId: number | null;
+  } | null;
+}
+
+const main = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async ({ req }): Promise<Context> => {
+      const userInfo = await jwtHelper.getUserInfoFromToken(
+        req.headers.authorization as string
+      );
+      return {
+        prisma,
+        userInfo,
+      };
+    },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+main();
